16412dcef11bc93fb960d9f98327ab31f7fed4be
wes337 <wesmoses@gmail.com>
2022-09-22 18:13:03 +0300

    fix

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -69,7 +69,7 @@ Or maybe you'd like some tailored ++ MyNewsBytes? ++ Eat a bag of sponsored chip
 const defaultValue = `\"Awesome! PLEASE r*** my mind again!!!\"\n\n ThiS amazing invention is capable of intercepting any sensory data and digitally processing it, all for the low price of your eternal soul. This allows amazing achievements like, for example, drinkable advertisements. Every time you drink water, you'll be rewarded free sredits or gems deposited in your account after enjouing the complimentary advertisement viewing experience and fulfilling a quick consumer questionpaire!\n\nThe time dilation processor means that longform promotions such as advuementaries (documentary-length advertisements) can be fully experiensed in the blink of an eye! In your mipd, you just watched three hours of Liam Neeson explain why High Society is the best brand of Anal Freshness Inserts, while in real life mere seconds have passed. Or maybe you'd like some tailored MyNewsBytes? Eat a bag of sponsored chips and get a dose of the hottest headlines delivered directly to your cerebral cortex! Gope are the days of fumbling for the remote to keep up with twelve different sitcoms and three pews stations simultapeously on the 360-degree PeEnCu (Personal Entertainment Cube) - DomeHomie gives you what YOU want, straight to the dome!\n\nPermiTech: One (1) enemy card becomes  Fake Tech faction. Use tactically.\n\nPaid Programming: One (1) INT 3 or lower Opponent creature cannot be used unless they are paired with a Product and/or Service We Do EnjoyTM card.`;
 
 const Input = () => {
-  const [value, setValue] = useState(defaultValue);
+  const [value, setValue] = useState(id2);
   const setHtml = useStore(({ setHtml }) => setHtml);
 
   const updateHtml = useCallback(() => {
